adb10afadaa98af15d48e920b8845ed681584365
hortensia <824466875@qq.com>
2023-11-24 19:44:43 +0800

    modulate

--- src/lib.rs
+++ src/lib.rs
@@ -55,18 +55,27 @@ impl Packet {
             })
     }
 
-    fn seal_one(self: Self) -> Vec<u8> {
+    fn seal_one(self: &Self) -> Vec<u8> {
         let mut packet = Vec::new();
         packet.extend_from_slice(&self.order.to_le_bytes());
         packet.extend_from_slice(&self.data.len().to_le_bytes());
+        packet.extend_from_slice(&self.data);
         packet
     }
 
-    pub fn seal(s: &[u8]) -> Vec<Vec<u8>> {
-        Self::new_packets(s)
-            .into_iter()
-            .map(Self::seal_one)
-            .collect()
+    pub fn seal(s: &[Packet]) -> Vec<Vec<u8>> {
+        s.into_iter().map(Self::seal_one).collect()
+    }
+
+    fn unseal_one(v: &[u8]) -> Self {
+        let order = usize::from_le_bytes(v[0..8].try_into().unwrap());
+        let len = usize::from_le_bytes(v[8..16].try_into().unwrap());
+        let data = v[16..16 + len].to_vec();
+        Self { order, data }
+    }
+
+    pub fn unseal(v: &[Vec<u8>]) -> Vec<Packet> {
+        v.into_iter().map(|x| Self::unseal_one(x)).collect()
     }
 }
 
@@ -78,14 +87,111 @@ fn pack_unpack_test() {
     assert_eq!(data, decode(&unpacked));
 }
 
-// use dasp::{signal, Signal};
+#[test]
+fn pack_unseal_test() {
+    let data = "hello world";
+    let packets = Packet::new_packets(&encode(data));
+    let sealed = Packet::seal(&packets);
+    let unsealed = Packet::unseal(&sealed);
+    let unpacked = Packet::unpack(&unsealed);
+    assert_eq!(data, decode(&unpacked));
+}
+
+use std::sync::Mutex;
+
+use dasp::{signal, Signal};
 
 /// Generate sound wave to carry the information.
-pub fn modulate(_: Vec<Vec<u8>>) -> Vec<f64> {
-    // signal::rate(44100.0)
-    // .const_hz(440.0)
-    // .sine()
-    // .take(10)
-    // .collect::<Vec<f64>>();
-    todo!("modulate");
+/// For first version, I will just use BPSK modulation.
+pub fn modulate(segments: Vec<Vec<u8>>) -> Vec<f64> {
+    segments
+        .into_iter()
+        .map(modulate_vector)
+        .flatten()
+        .collect()
 }
+
+fn modulate_vector(p: Vec<u8>) -> Vec<f64> {
+    p.into_iter().map(modulate_byte).flatten().collect()
+}
+
+fn modulate_byte(b: u8) -> Vec<f64> {
+    (0..8)
+        .map(|i| modulate_bit(b & (1 << i) >> i))
+        .flatten()
+        .collect()
+}
+
+use once_cell::sync::Lazy;
+
+static ZERO_SIGNAL: Lazy<Mutex<Vec<f64>>> = Lazy::new(|| {
+    Mutex::new(
+        signal::rate(44100.0)
+            .const_hz(440.0)
+            .phase()
+            .sine()
+            .take(4400)
+            .collect::<Vec<f64>>(),
+    )
+});
+
+fn modulate_bit(b: u8) -> Vec<f64> {
+    match b {
+        0 => ZERO_SIGNAL.lock().unwrap().clone(),
+        1 => ZERO_SIGNAL
+            .lock()
+            .unwrap()
+            .iter()
+            .map(|x| -x)
+            .collect::<Vec<f64>>(),
+        _ => panic!("only 0 and 1 are allowed"),
+    }
+}
+
+#[test]
+fn test_modulate() {
+    let data = "hello world";
+    let modulated = modulate(Packet::seal(&Packet::new_packets(&encode(data))));
+    assert_eq!(modulated.len(), 4400 * 8 * (16 + 11));
+}
+
+use hound;
+/// output the sound wave to a wav file
+pub fn output_wav(modulated: &[f64], filename: &str) {
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Float,
+    };
+    let mut writer = hound::WavWriter::create(filename, spec).unwrap();
+    for sample in modulated {
+        writer.write_sample(*sample as f32).unwrap();
+    }
+    writer.finalize().unwrap();
+}
+
+#[test]
+fn test_output_wav() {
+    let data = "hello world";
+    let modulated = modulate(Packet::seal(&Packet::new_packets(&encode(data))));
+    output_wav(&modulated, "test.wav");
+}
+
+use hound::WavReader;
+/// read the sound wave from a wav file
+pub fn input_wav(filename: &str) -> Vec<f64> {
+    let mut reader = WavReader::open(filename).unwrap();
+    let samples: Vec<f64> = reader.samples::<f32>().map(|x| x.unwrap() as f64).collect();
+    samples
+}
+
+#[test]
+fn test_input_wav() {
+    let data = "hello world";
+    let modulated = modulate(Packet::seal(&Packet::new_packets(&encode(data))));
+    output_wav(&modulated, "test.wav");
+    let input = input_wav("test.wav");
+    assert_eq!(modulated.len(), input.len());
+}
+
